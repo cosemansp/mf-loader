cef811d56a0cf49bf1b419478b9f712c765d1c9e
Peter Cosemans <peter.cosemans@gmail.com>
2021-04-11 00:07:23 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-mf-loader
+# mf-loader
+
+Webpack Module Federation loader
+
+## vanillaJS
+
+```html
+<html lang="en">
+  <head>
+    <script src="https://cdn.jsdelivr.net/npm/@euricom/mf-loader@0.1.0"></script>
+  </head>
+  <body>
+    <h1>Vanilla JS</h1>
+    <div id="panel"></div>
+    <script>
+      mfLoader.init('app@http://localhost:3000/remoteEntry.js', 'shareable/Panel', { selector: '#panel' });
+    </script>
+  </body>
+</html>
+```
---
+++ src/index.tsx
@@ -0,0 +1,55 @@
+interface Module {
+  mount: (module: any, options: any) => void;
+}
+interface Container {
+  init: (module: any) => void;
+  get: (modelName: string) => () => Module;
+}
+declare function __webpack_init_sharing__(scope: string): void;
+declare let __webpack_share_scopes__: any;
+
+const init = (remote: string, moduleName: string, options) => {
+  const { selector } = options || {};
+
+  if (!selector) {
+    throw Error('Selector required');
+  }
+
+  const element = document.querySelector(selector);
+  if (!element) {
+    throw Error(`Element not found: ${selector}`);
+  }
+
+  const appParts = remote.split('@');
+  const appName = appParts[0];
+  const url = appParts[1];
+  if (!appName || !url) {
+    throw Error('Invalid remote');
+  }
+
+  const remoteScript = document.createElement('script');
+  remoteScript.setAttribute('src', url);
+  document.head.appendChild(remoteScript);
+
+  remoteScript.addEventListener('load', async () => {
+    await __webpack_init_sharing__('default');
+
+    const container = window[appName] as Container;
+    if (!container) {
+      throw Error(`Remote not found: ${appName}`);
+    }
+    await container.init(__webpack_share_scopes__.default);
+
+    const factoryFn = await container.get(moduleName);
+    const module = factoryFn();
+    module.mount(element, options);
+  });
+};
+
+export default {
+  init,
+};
+
+// (window as any).loader = {
+//   init,
+// };
